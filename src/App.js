0a563491e89f1f00ab1402b1f611140143282660
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-07-11 23:39:47 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Todolist from './components/Todolist.jsx';
+import Todolist from './components/Todolist';
 
 function App() {
   return (
--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import TodosLogic from './TodosLogic';
-// import InputField from './input';
-// import ListSection from './listSection';
 
 const Todolist = () => (
   <div className="App">
     <Header />
     <TodosLogic />
-    {/* <Addtodo />
-    <ListSection /> */}
+    {}
   </div>
 );
 
